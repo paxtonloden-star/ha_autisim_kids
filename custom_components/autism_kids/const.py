abda88b05359374145dba037b4cb917cfa482a7f
paxtonloden-star <paxtonloden@gmail.com>
2026-04-01 22:29:20 -0500

    Add integration constants

---
+++ custom_components/autism_kids/const.py
@@ -0,0 +1,23 @@
+DOMAIN = "autism_kids"
+PLATFORMS = ["sensor"]
+
+CONF_KID_CALENDAR = "kid_calendar"
+CONF_FAMILY_CALENDAR = "family_calendar"
+CONF_PERSON_ONE = "person_one"
+CONF_PERSON_ONE_NAME = "person_one_name"
+CONF_PERSON_TWO = "person_two"
+CONF_PERSON_TWO_NAME = "person_two_name"
+CONF_WEATHER = "weather"
+CONF_SPECIAL_CHANGE_TEXT = "special_change_text"
+CONF_DINNER_TEXT = "dinner_text"
+CONF_BEDTIME_HELPER = "bedtime_helper"
+CONF_SCHOOL_TOMORROW = "school_tomorrow"
+CONF_NOW_FALLBACK = "now_fallback"
+CONF_NEXT_FALLBACK = "next_fallback"
+CONF_LATER_FALLBACK = "later_fallback"
+
+DEFAULT_PERSON_ONE_NAME = "Mom"
+DEFAULT_PERSON_TWO_NAME = "Dad"
+DEFAULT_NOW_FALLBACK = "Free Time"
+DEFAULT_NEXT_FALLBACK = "Check Board"
+DEFAULT_LATER_FALLBACK = "Dinner"
